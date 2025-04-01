78e05bd65ce96e4931d23896737b47208e4c6274
Alina Prystynska <pristinskayaalina9@gmail.com>
2025-04-01 13:39:43 +0100

    1/04

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ app.use(cors({
 }));
 
 // Set up port for server to listen on
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 8000;
 
 // ! [IMPORTANT]: Replace with your mongoDB URI string. You can get it from your Atlas cluster.
 //const uri ="mongodb+srv://Alina:300588Alina@cluster0.hg1jotf.mongodb.net/car?retryWrites=true&w=majority&appName=Cluster0"
+
 const uri = process.env.MONGO_URI;
 
 // Connect to the database
@@ -28,6 +29,8 @@ mongoose.connect(uri, { useNewUrlParser: true }).then(
   () => { console.log('Successfully connected to the database!') },
   err => { console.log('Could not connect to the database...' + err) }
 );
+
+
 
 // Allow app to accept json and url encoded values
 app.use(bodyParser.urlencoded({ extended: true }));
